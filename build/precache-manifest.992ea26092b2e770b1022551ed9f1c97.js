a5d8ab16cf0bd619ec7846d2470b282aae8b07bc
Julius <jag163@ucsd.edu>
2020-03-04 13:39:36 -0800

    deployment to heroku

---
+++ build/precache-manifest.992ea26092b2e770b1022551ed9f1c97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9614eb4395cb19a6856bc258141b4a3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5139ccc753819ed1cc8",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "c3429b54357a5e52c7ee",
+    "url": "/static/css/main.f16fc812.chunk.css"
+  },
+  {
+    "revision": "b5139ccc753819ed1cc8",
+    "url": "/static/js/2.a92904d9.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.a92904d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3429b54357a5e52c7ee",
+    "url": "/static/js/main.818211e4.chunk.js"
+  },
+  {
+    "revision": "3d0235b3ba66d91ef422",
+    "url": "/static/js/runtime-main.bdbdfb9a.js"
+  }
+]);
