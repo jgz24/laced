985d2770b2fcad0a3dd1f8ef6698e3238ca72441
Julius <jag163@ucsd.edu>
2020-03-04 20:20:30 -0800

    Fixed css styling issues.

--- src/routes/CheckOutRoute.js
+++
@@ -1,8 +0,0 @@
-const { makePayment } = require("../controllers/PaymentController");
-
-const express = require("express");
-const router = express.Router();
-
-router.post("/", makePayment);
-
-module.exports = router;
--- src/routes/ShoeRoute.js
+++
@@ -1,8 +0,0 @@
-const { addShoe } = require("../controllers/ShoeController");
-
-const express = require("express");
-const router = express.Router();
-
-router.post("/post", addShoe);
-
-module.exports = router;
